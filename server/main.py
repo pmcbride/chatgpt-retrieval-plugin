433841f18fca4477d5141d24c13eada448bb0349
pmcbride <pmcbride@github.com>
2023-09-08 04:27:49 -0700

    Add SimpleAIChat and tools to plugin

--- server/main.py
+++ server/main.py
@@ -28,6 +28,53 @@ bearer_scheme = HTTPBearer()
 BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
 assert BEARER_TOKEN is not None
 
+# --- Simple AI Chat Code Below ---
+
+# import quart
+# import quart_cors
+# from quart import request, jsonify
+from simpleaichat import AIChat
+from tools import load_tools
+from typing import Any, Dict, List, Optional, Union
+
+import json
+import pprint as pp
+from pprint import PrettyPrinter, pprint, pformat
+pprint = pp.PrettyPrinter(sort_dicts=False).pprint
+pformat = pp.PrettyPrinter(sort_dicts=False).pformat
+
+from rich.console import Console
+console = Console()
+TOOL_NAMES = [
+    "search",
+    "lookup"
+]
+
+TOOLS = load_tools(TOOL_NAMES)
+
+PARAMS = {
+    "temperature": 0.0,
+    "max_tokens": 100
+}
+AI_CHAT_ID = "simpleaichat-plugin"
+
+AI = AIChat(params=PARAMS, console=False, id=AI_CHAT_ID)
+print_ai(AI)
+
+# Create a Quart application with CORS enabled
+app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
+
+
+@app.route('/chat', methods=['POST'])
+async def chat():
+    global AI
+    data = await request.get_json()
+    query = data.get('query')
+    # Create AIChat instance
+    response = AI(query, tools=TOOLS, id=AI_CHAT_ID)
+    console_print(response, title=f"Response: \"{query}\"")
+    print_ai(AI)
+    return jsonify({'response': response})
 
 def validate_token(credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme)):
     if credentials.scheme != "Bearer" or credentials.credentials != BEARER_TOKEN:
